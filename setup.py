8597455af64c58e0c4f92c8c938c6cd7ff9bd9ae
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2020-05-28 08:29:04 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='morelia-noctua',
+    version='0.0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='Patrick Westphal',
+    author_email='patrick.westphal@informatik.uni-leipzig.de',
+    description='',
+    install_requires=[
+        'rdflib==5.0.0',
+        'pyparsing==2.4.7',
+    ]
+)
